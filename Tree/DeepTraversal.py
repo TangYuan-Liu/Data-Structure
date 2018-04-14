6b9a1266a8e74c599efdbf1ce17bc2d4e04abc4f
Mr.SY <1131165566@qq.com>
2018-04-14 17:55:27 +0800

    Update DeepTraversal.py

--- Tree/DeepTraversal.py
+++ Tree/DeepTraversal.py
@@ -1,1 +1,18 @@
 #树的深度遍历
+import TreeLayerTraversal as LS
+
+
+def DeepSearch_middle(head):
+    if(head.left != None):
+        DeepSearch_middle(head.left)
+    if(head.value != None):
+        print head.value
+    if(head.right != None):
+        DeepSearch_middle(head.right)
+
+
+
+if __name__ == "__main__":
+    head = LS.tree_node(None)
+    LS.create_tree(head)
+    DeepSearch_middle(head)
