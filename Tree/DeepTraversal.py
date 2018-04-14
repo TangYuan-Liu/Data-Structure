c7f94b2a483229766fd26814d5c580b9989858cc
Mr.SY <1131165566@qq.com>
2018-04-14 18:04:54 +0800

    Update DeepTraversal.py

--- Tree/DeepTraversal.py
+++ Tree/DeepTraversal.py
@@ -10,9 +10,29 @@ def DeepSearch_middle(head):
     if(head.right != None):
         DeepSearch_middle(head.right)
 
+def DeepSearch_preorder(head):
+    if(head.value != None):
+        print head.value
+    if(head.left != None):
+        DeepSearch_preorder(head.left)
+    if(head.right != None):
+        DeepSearch_preorder(head.right)
 
+def DeepSearch_postorder(head):
+    
+    if(head.left != None):
+        DeepSearch_postorder(head.left)
+    if(head.right != None):
+        DeepSearch_postorder(head.right)
+    if(head.value != None):
+        print head.value
 
 if __name__ == "__main__":
     head = LS.tree_node(None)
     LS.create_tree(head)
+    print("Middle traversal is:")
     DeepSearch_middle(head)
+    print("Preorder traversal is:")
+    DeepSearch_preorder(head)
+    print("Postorder traversal is:")
+    DeepSearch_postorder(head)
