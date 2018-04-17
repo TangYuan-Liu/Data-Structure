fe74e817b8a88522a9c3206b25681733e8ffcdd4
Mr.SY <1131165566@qq.com>
2018-04-17 21:23:11 +0800

    Update BST.py

--- Tree/BST.py
+++ Tree/BST.py
@@ -32,7 +32,53 @@ def CreateTree(head):
         print("Continue? Yes(1) No(0)")
         flag = raw_input()
 
+def FindMin(head):
+    if(head.value == None):
+        return None
+    else:
+        temp = head.value
+        NodePoint = head
+        
+        while(NodePoint.left != None):
+            NodePoint = NodePoint.left 
+        return NodePoint
+
+def DeleteNode(head,value):
+    if(head.value == None):
+        print("Can't find the node")
+        return head
+    else:
+        if(value > head.value):
+            head.right = DeleteNode(head.right,value)
+        else:
+            if(value < head.value):
+                head.left = DeleteNode(head.left,value)
+            else:
+                if(head.left != None and head.right != None):
+                    temp = FindMin(head.right)
+                    head.value = temp.value
+                    head.right = DeleteNode(head.right,temp.value)
+                else:
+                    if(head.left == None):
+                        head = head.right
+                    else:
+                        head = head.left
+        return head
+                        
+
+
 if __name__ == "__main__":
     head = TL.tree_node(None)
     CreateTree(head)
+    TL.output_tree(head)
+    MinNode = FindMin(head)
+    flag = None
+    print("Do you want start to delete? Yes(1) No(0)")
+    flag = raw_input()
+    while(flag == '1'):
+        print("Which node want to delete?")
+        temp = int(raw_input())
+        DeleteNode(head,temp)
+        print("Continue? Yes(1) No(0)")
+        flag = raw_input()
     TL.output_tree(head)
